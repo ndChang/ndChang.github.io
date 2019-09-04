33dbbc9d064b669c6e087a1aef52448cf0562f21
ndChang <52163387+ndChang@users.noreply.github.com>
2019-09-04 18:02:26 -0400

    test functionality

--- JS/retirement.js
+++ JS/retirement.js
@@ -15,32 +15,91 @@
 
 
 function onReady() {
-    let currentIndex = 0;
-    const rating = [ 0, 0, 0, 0, 0];
-    const images = [1, 2, 3, 4, 5];
-    let imgSwap = $('');
+    let userAge;
+    let userWealth;
+    let userSaving;
+    let userGrowth;
+    let retirementAge = 65;
 
+    console.log(userAge)
     function onAgeSubmit(event) {
         event.preventDefault();
-        console.log(event);
+        userAge = parseInt($('#age').val());
+        // console.log(userAge)
     }
     function onWealthSubmit(event) {
         event.preventDefault();
-        console.log(event);
+        userWealth = parseInt($('#wealth').val());
+        // console.log(userWealth)
     }
     function onSavingSubmit(event) {
         event.preventDefault();
-        console.log(event);
+        userSaving = parseInt($('#saving').val());
+        // console.log(userSaving);
     }
     function onGrowthSubmit(event){
-        // event.preventDefault();
-        console.log(event);
+        event.preventDefault();
+        userGrowth = parseInt($('#growth').val());
+        userGrowth/=100;
+        // console.log(userGrowth);
+        futureCalculation();
     }
+    function futureCalculation() {
+        let timeUntilRetirement = retirementAge - userAge;
+        // userWealth = userSaving * (1 + userGrowth)**(timeUntilRetirement);
+        for (let i = 0; i<=timeUntilRetirement; i++) {
+            const futureAge = [];
+            const futureWealth = [];
+            userWealth = userSaving * (1 + userGrowth)**(i);
+            userWealth = parseFloat(userWealth).toFixed(2);
+            futureAge[i] = i + userAge;
+            futureWealth[i] = "$" + userWealth;
+            // $('#saving-log').append('<p class="table">' + futureAge[i] +" "+ futureWealth[i] + '</p>');
+            dataPoints.y = futureWealth[i]
+        }
+        console.log(futureAge, futureWealth);
+    }
+    // Datapoints for graph
+    let dataPoints = [];
+    // Constructor for Datapoints (y, futureWealth[i])
+    const dataPoint = function createDataPoint(y, futureWealth, index) {
+        let graphPoint = {
+            this.y: futureWealth,
+        }
+    }
+    // line chart
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        theme: "light2",
+        title:{
+            text: "Savings Growth"
+        },
+        axisY:{
+            includeZero: false
+        },
+        data: [{        
+            type: "line",       
+            dataPoints: [ //load from var dataPoints
+                // { y: 450 },
+                // { y: 414},
+                // { y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
+                // { y: 460 },
+                // { y: 450 },
+                // { y: 500 },
+                // { y: 480 },
+                // { y: 480 },
+                // { y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+                // { y: 500 },
+                // { y: 480 },
+                // { y: 510 }
+            ]
+        }]
+    });
+    chart.render();
 
-    $('#age').submit(onAgeSubmit);
-    $('#wealth').submit(onWealthSubmit);
-    $('#saving').submit(onSavingSubmit);
-    $('#growth').submit(onGrowthSubmit);
-
+    $('#user-info').submit(onAgeSubmit);
+    $('#user-info').submit(onWealthSubmit);
+    $('#user-info').submit(onSavingSubmit);
+    $('#user-info').submit(onGrowthSubmit);
 }
 $('document').ready(onReady);
